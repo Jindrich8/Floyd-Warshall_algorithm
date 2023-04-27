ceee34c452ad96354af84fc9f2f7a28385673c1e
Jindrich8 <jindra.matematik@gmail.com>
2023-04-27 08:22:12 +0200

    Example change

--- src/Floyd-Warshall_algorithm/Program.cs
+++ src/Floyd-Warshall_algorithm/Program.cs
@@ -2,24 +2,30 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            double[,] graph =
-            { {0,   5,  double.PositiveInfinity, 10},
-                               {double.PositiveInfinity,  0,  3,  double.PositiveInfinity},
-                               {double.PositiveInfinity, double.PositiveInfinity, 0,   1},
-                               {double.PositiveInfinity, double.PositiveInfinity, double.PositiveInfinity, 0} };
+            const double inf = double.PositiveInfinity;
+            double[,] graph = {
+                { 0, inf, inf, inf, inf, inf },
+{ 3, 0, 5, inf, inf, inf },
+{ inf, inf, 0, inf, inf, 4 },
+{  -4, inf, inf, 0, inf, inf },
+{ inf, inf, 1,  inf, 0, inf },
+{ inf, inf, inf, 6, 3, 0} };
 
-            bool[,] graph2 =
-           { {true,   true,  false, true},
-                               {false,  true,  true,  false},
-                               {false, false, true,   true},
-                               {false, false, false, true} };
+            bool[,] graph2 ={
+                { true, false, false, false, false, false },
+{ true, true, true, false, false, false },
+{ false, false, true, false, false, true },
+{  true, false, false, true, false, false },
+{ false, false, true,  false, true, false },
+{ false, false, false, true, true, true} };
 
-           var floyd = new GraphFloyd(graph);
+            var floyd = new GraphFloyd(graph);
             floyd.Floyd();
             floyd.Print(Console.Out);
             Console.WriteLine();
+
             var roy = new GraphRoy(graph2);
             roy.Roy();
             roy.Print(Console.Out);
